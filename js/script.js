b42f239680d5cae8033550ffb879c6d23eaa3b11
VladNcode <vldsmrfow@gmail.com>
2021-10-04 13:13:16 +0300

    Added form submittion

--- js/script.js
+++ js/script.js
@@ -143,7 +143,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// const modalTimer = setTimeout(showModal, 50000000);
+const modalTimer = setTimeout(showModal, 5000000000000);
 
 const showModalByScroll = function () {
   if (
@@ -261,3 +261,57 @@ new MenuCard(
 
 // const div = new MenuCard();
 // div.render();
+
+// Forms
+
+const forms = document.querySelectorAll('form');
+
+const message = {
+  loading: 'Loaded!',
+  success: 'Thanks, we will contact you ASAP!',
+  failure: 'Something went wrong!',
+};
+
+forms.forEach(form => {
+  postData(form);
+});
+
+function postData(form) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+    statusMessage.textContent = message.loading;
+    form.append(statusMessage);
+
+    const request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+
+    request.setRequestHeader('Content-type', 'application/json');
+    const formData = new FormData(form);
+
+    const obj = {};
+
+    formData.forEach(function (value, key) {
+      obj[key] = value;
+    });
+
+    const json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('load', function () {
+      if (request.status === 200) {
+        console.log(request.response);
+        statusMessage.textContent = message.success;
+        form.reset();
+        setTimeout(function () {
+          statusMessage.remove();
+        }, 2000);
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+  });
+}
